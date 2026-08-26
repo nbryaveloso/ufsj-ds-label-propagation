fd39d928930fa01d9773db77167d8af055d8c5c3
Bryan Veloso <bryanveloso.dev@gmail.com>
2026-08-26 17:27:59 -0300

    refactor: modulariza logica de plotagem do grafico

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import networkx as nx
 import csv
+import matplotlib.pyplot as plt
 
 def carregar_grafo(caminho_arquivo):
     # Funcao para ler o csv
@@ -52,6 +53,27 @@ def propagar_rotulos(A, N, max_iteracoes=100):
         
     return rotulos, iteracao
 
+def plotar_comunidades(G, rotulos_finais, caminho):
+    # Funcao que gera e exibe o grafico colorido das comunidades.
+    print("\nGráfico gerado. Feche-o para encerrar.")
+    
+    plt.figure(figsize=(8, 6))
+    
+    nome_arquivo = caminho.split('/')[-1] # Extrai apenas o nome do arquivo.
+    plt.title(f"Label Propagation - {nome_arquivo}")
+    
+    # Plota o grafo usando os rotulos para definir as cores.
+    nx.draw(
+        G, 
+        with_labels=True, 
+        node_color=rotulos_finais, 
+        cmap=plt.cm.Set2,
+        node_size=800,
+        font_color="black",
+        font_weight="bold"
+    )
+    plt.show()
+
 if __name__ == "__main__":
     caminho = 'data/rede1_duas_comunidades.csv'
     print(f"Processando: {caminho}")
@@ -66,3 +88,6 @@ if __name__ == "__main__":
     
     print(f"\nConvergência alcançada em {total_iteracoes} iterações.")
     print("Rótulos Finais (Comunidades):", rotulos_finais)
+
+    # Chama a funcao de visualizacao
+    plotar_comunidades(G, rotulos_finais, caminho)
